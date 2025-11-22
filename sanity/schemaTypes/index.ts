2b9fc921f49d68ff5f030f1b996c6d6f2e70d6c2
Dudeism74 <trustedessentialsgpt@gmail.com>
2025-11-22 06:09:21 -0500

    Added Article Schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import post from './post'
+import author from './author'
+import category from './category'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [post, author, category],
 }
