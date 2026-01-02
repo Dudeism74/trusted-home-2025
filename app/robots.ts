d0b6c56f57d8bbda28ed3e55ab1787da9667a9e6
Dudeism74 <trustedessentialsgpt@gmail.com>
2026-01-01 21:14:40 -0500

    Update sitemap URL to use 'www' prefix

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: "*",
             allow: "/",
         },
-        sitemap: "https://trustedhomeessentials.com/sitemap.xml",
+        sitemap: "https://www.trustedhomeessentials.com/sitemap.xml",
     };
 }
