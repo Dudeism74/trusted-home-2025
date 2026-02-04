0006791474685375265e7762f97ebdf15b309228
Dudeism74 <trustedessentialsgpt@gmail.com>
2026-02-04 16:43:12 -0500

    Add disallow rules for feed URLs in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,8 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
+            // THIS IS THE MISSING PART: Block the feeds so Google ignores them
+            disallow: ["/feed/", "/*/feed/", "/comments/feed/"],
         },
         sitemap: "https://www.trustedhomeessentials.com/sitemap.xml",
     };
