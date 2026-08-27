dcd19703d8410147772a2f60e1f4e06acff42b4c
Dudeism74 <trustedessentialsgpt@gmail.com>
2026-08-27 16:34:58 -0400

    Add refrigerator not cooling page

---
+++ app/refrigerator-not-cooling/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import { ResourcePage } from "../components/resource-page";
+import { getAdditionalResource } from "../lib/additional-resources";
+
+const resource = getAdditionalResource("refrigerator-not-cooling")!;
+
+export const metadata: Metadata = {
+  title: resource.metaTitle,
+  description: resource.metaDescription,
+  alternates: { canonical: `/${resource.slug}` },
+};
+
+export default function RefrigeratorNotCoolingPage() {
+  return <ResourcePage resource={resource} />;
+}
